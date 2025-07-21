98237616c539f423ad02239366f6431c944ed4b6
skuralll <fuji.ktbl@gmail.com>
2025-07-21 21:13:46 +0900

    Storageインタフェースを追加

--- internal/repository/storage.go
+++ internal/repository/storage.go
@@ -1,1 +1,17 @@
 package repository
+
+import permission "github.com/skuralll/df-permission"
+
+type Storage interface {
+	// ストレージバックエンドからPermissionDataを取得して返す
+	Load() (*permission.PermissionData, error)
+
+	// PermissionDataをストレージバックエンドに保存
+	Save(data *permission.PermissionData) error
+
+	// ストレージバックエンドに権限データが存在するか確認
+	Exists() bool
+
+	// 必要に応じてストレージバックエンドを安全に終了させる
+	Close() error
+}
