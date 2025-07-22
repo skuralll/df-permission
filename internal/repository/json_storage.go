d35155d952624987590b87d2f48436d447a3ec8a
skuralll <fuji.ktbl@gmail.com>
2025-07-22 22:21:54 +0900

    コンストラクタ関数と各関数のスケルトンを追加

--- internal/repository/json_storage.go
+++ internal/repository/json_storage.go
@@ -11,4 +11,31 @@ type JSONStorage struct {
 	mutex  sync.RWMutex
 }
 
-var _ JSONStorage = Storage{}
+var _ Storage = (*JSONStorage)(nil)
+
+func NewJSONStorage(config permission.StorageConfig) *JSONStorage {
+	return &JSONStorage{
+		config: config,
+		mutex:  sync.RWMutex{},
+	}
+}
+
+// Close implements Storage.
+func (j *JSONStorage) Close() error {
+	panic("unimplemented")
+}
+
+// Exists implements Storage.
+func (j *JSONStorage) Exists() bool {
+	panic("unimplemented")
+}
+
+// Load implements Storage.
+func (j *JSONStorage) Load() (*permission.PermissionData, error) {
+	panic("unimplemented")
+}
+
+// Save implements Storage.
+func (j *JSONStorage) Save(data *permission.PermissionData) error {
+	panic("unimplemented")
+}
