4147d83ee9ca5646abb162c806cda6698bf22bc9
skuralll <fuji.ktbl@gmail.com>
2025-07-23 21:36:22 +0900

    JsonStorage.Save()を実装

--- internal/repository/json_storage.go
+++ internal/repository/json_storage.go
@@ -3,7 +3,9 @@ package repository
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 	"sync"
+	"time"
 
 	permission "github.com/skuralll/df-permission"
 )
@@ -61,10 +63,62 @@ func (j *JSONStorage) Exists() bool {
 
 // Save implements Storage.
 func (j *JSONStorage) Save(data *permission.PermissionData) error {
-	panic("unimplemented")
+	j.mutex.Lock()
+	defer j.mutex.Unlock()
+
+	// バリデーション
+	if err := ValidatePermissionData(data); err != nil {
+		return NewStorageError("validation", err.Error())
+	}
+
+	// ディレクトリの存在確認, なければ作成
+	dir := filepath.Dir(j.config.Path)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return NewStorageError("mkdir", err.Error())
+	}
+
+	// メタデータの更新
+	data.Meta.UpdatedAt = time.Now()
+	if data.Meta.CreatedAt.IsZero() {
+		data.Meta.CreatedAt = time.Now()
+	}
+
+	// JSONへ変換
+	jsonData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return NewStorageError("marshal", err.Error())
+	}
+
+	// 一時ファイルを使用してAtomicに保存
+	tempPath := j.config.Path + ".tmp"
+	if err := os.WriteFile(tempPath, jsonData, 0644); err != nil {
+		return NewStorageError("write", err.Error())
+	}
+	if err := os.Rename(tempPath, j.config.Path); err != nil {
+		os.Remove(tempPath)
+		return NewStorageError("rename", err.Error())
+	}
+
+	// ディスクに強制的に書き込む
+	if err := j.syncFile(); err != nil {
+		return NewStorageError("sync", err.Error())
+	}
+
+	return nil
 }
 
 // Close implements Storage.
 func (j *JSONStorage) Close() error {
 	panic("unimplemented")
+}
+
+// ディスクに強制的に書き込む
+func (j *JSONStorage) syncFile() error {
+	file, err := os.OpenFile(j.config.Path, os.O_RDONLY, 0)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return file.Sync()
 }
