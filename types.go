a784573ab28d675e883e07105880a5b4dcaaa24f
skuralll <fuji.ktbl@gmail.com>
2025-07-27 01:24:38 +0900

    types.goを和訳

--- types.go
+++ types.go
@@ -2,7 +2,7 @@ package dfpermission
 
 import "github.com/skuralll/df-permission/internal/shared"
 
-// Re-export configuration types from internal/shared for public API
+// 公開API用にinternal/sharedから設定型を再エクスポート
 type (
 	StorageConfig = shared.StorageConfig
 	CacheConfig   = shared.CacheConfig
