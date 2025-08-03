817b75d6bcf42d78b71a2a0049176c8ba6199191
skuralll <fuji.ktbl@gmail.com>
2025-08-03 20:44:38 +0900

    参加時のイベントハンドラをManagerのメソッドに変更

--- internal/application/events.go
+++ internal/application/events.go
@@ -1,13 +1,21 @@
 package application
 
-import "github.com/google/uuid"
+import (
+	"errors"
 
-// // 参加時のイベントハンドラ
-// func (mgr *Manager) OnPlayerJoin(playerID uuid.UUID, defaultGroup string) error {
-// 	// キャッシュ削除処理
-// 	mgr.cache.InvalidatePlayer(playerID)
-// 	return nil
-// }
+	"github.com/google/uuid"
+	"github.com/skuralll/df-permission/internal/shared"
+)
+
+// 参加時のイベントハンドラ
+func (mgr *Manager) OnPlayerJoin(playerID uuid.UUID, playerName string) error {
+	// 新しいプレイヤーを登録（既存なら無視）
+	err := mgr.CreatePlayer(playerID, playerName)
+	if err != nil && !errors.Is(err, shared.ErrPlayerAlreadyExists) {
+		return err
+	}
+	return nil
+}
 
 // 退出時のイベントハンドラ
 func (mgr *Manager) OnPlayerLeave(playerID uuid.UUID) error {
